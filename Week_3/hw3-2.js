50ccdc2b4015938599c09fb4f3574af2f75b2b5a
Ratithoglys <github.com@ebumna.net>
2013-10-18 23:01:47 +0200

    Update hw3-2.js

--- Week_3/hw3-2.js
+++ Week_3/hw3-2.js
@@ -26,5 +26,8 @@ db.zips.count()
 Now check in a similar manner the "city" field for its first character. You will find there are cities in the collection that start with a number -- which indicates either a noisy data set or zip codes that have no corresponding city. Let's suppose we want to delete these zip code documents -- the ones that have a numeric city name -- from our data set. Do a remove operation to delete those documents.
 After removing the documents indicated above, how many documents remain in the zips collection?
 
+> db.zips.aggregate(   [     { $project : { _id : { $substr : ["$city",0,1] } } } ,      { $group : { _id : "$_id", n : {$sum:1} } },{$match : {_id:{$gte :"0"}, _id:{$lte : "9"} } }, {$sort: {_id:1}}   ] )
+> db.zips.remove({city: /^[0-9]/})
+> db.zips.count()
 
 answer: 29353 		
