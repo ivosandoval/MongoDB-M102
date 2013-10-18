fce96aa88b026a7e1a290520b8b367d437324391
Ratithoglys <github.com@ebumna.net>
2013-10-18 22:58:08 +0200

    Update hw3-1.js

--- Week_3/hw3-1.js
+++ Week_3/hw3-1.js
@@ -9,10 +9,11 @@ MD5 (zips.json) = 4854d69c2ac389f334c0abff03d96259
 $
 $ # now import it ...
 
-mongo
+> mongoimport --db pcat --collection zips < zips.json
 > db.zips.count()
 29467
 
 Question: consider the state with the 4th most zip codes in it. How many zip codes does that state have? Use the aggregation framework to query this.
 
+> db.zips.aggregate( { $group : { _id : "$state", countZips : { $sum : 1 } }}, { $sort: {countZips:-1}}, {$limit:4} );
 answer: 1458
